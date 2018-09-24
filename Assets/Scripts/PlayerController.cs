f3a722f103ca0f681f630e1d482758ba5d9e104d
cody000125 <32820048+cody000125@users.noreply.github.com>
2018-09-23 22:58:17 -0700

    RollABallGame

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rb;
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+
+        rb.AddForce(movement * speed);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+            audio.Play();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 13)
+        {
+            winText.text = "You Win";
+        }
+    }
+}
